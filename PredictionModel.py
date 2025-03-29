0150d81aa2467a29e8d14323de79885c3dad82fb
SamuelRamirezG <111009060+SamuelRamirezG@users.noreply.github.com>
2025-03-29 17:57:08 -0500

    cambiosaaa

--- PredictionModel.py
+++ PredictionModel.py
@@ -1,9 +1,105 @@
+import re
+
+import pandas as pd
+
+nlp = spacy.load("es_core_news_sm")
+
+import unicodedata
+
+import num2words
+
+import spacy
+
+def limpiar_ascii(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: unicodedata.normalize("NFKD", x).encode("ascii", "ignore").decode("utf-8"))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: unicodedata.normalize("NFKD", x).encode("ascii", "ignore").decode("utf-8"))
+
+    return
+
+
+def limpiar_mayusculas(train):
+
+    train["Titulo"] = train["Titulo"].str.lower()
+    train["Descripcion"] = train["Descripcion"].str.lower()
+
+    return
+
+
+def limpiar_puntuacion(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: re.sub(r'[.:,;-_¿?¡!"\'()\[\]{}\/%\$@+*\^]', '', x))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: re.sub(r'[.:,;-_¿?¡!"\'()\[\]{}\/%\$@+*\^]', '', x))
+    
+    return
+
+
+def limpiar_numeros(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: re.sub(r'\b\d+\b', lambda y: num2words.num2words(y.group(), lang='es'), x))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: re.sub(r'\b\d+\b', lambda y: num2words.num2words(y.group(), lang='es'), x))
+
+    return
+
+
+palabras_stop = nlp.Defaults.stop_words
+
+palabras_s = r'\b(?:' + '|'.join(palabras_stop) + r')\b'
+
+def limpiar_stopwords(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: re.sub(palabras_s, '', x))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: re.sub(palabras_s, '', x))
+
+    return
+
+def limpiar_guiones(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: x.replace("-", " "))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: x.replace("-", " "))
+
+    return
+
+numeros_post_ascii = r'[0123456789]'
+
+def limpiar_numeros_ascii(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: re.sub(numeros_post_ascii, '', x))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: re.sub(numeros_post_ascii, '', x))
+
+    return
+
+def limpiar_texto(train):
+
+    limpiar_ascii(train=train)
+    limpiar_mayusculas(train=train)
+    limpiar_puntuacion(train=train)
+    limpiar_numeros(train=train)
+    limpiar_stopwords(train=train)
+    limpiar_guiones(train=train)
+    limpiar_numeros_ascii(train=train)
+
+    return
+
+def tokenizar(train):
+
+    train["Titulo_T"] = train["Titulo"].swifter.allow_dask_on_strings(enable=True).apply(lambda x: [token.text for token in nlp(x) if not token.is_space])
+    train["Descripcion_T"] = train["Descripcion"].swifter.allow_dask_on_strings(enable=True).apply(lambda x: [token.text for token in nlp(x) if not token.is_space])
+
+    return train
+
+def unir_texto(train):
+
+    textos = train["Titulo_T"].astype(str) + " " + train["Descripcion_T"].astype(str)
+
+    return textos
+
 from joblib import load
 
 class Model:
 
     def __init__(self,columns):
-        self.model = load("modelo.joblib")
+        self.model = load("assets/model.joblib")
 
     def make_predictions(self, data):
         result = self.model.predict(data)
--- main.py
+++ main.py
@@ -4,13 +4,109 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 import io
 from sklearn.ensemble import VotingClassifier
 
-from joblib import load
-
 import pandas as pd
+
+import re
+
+import unicodedata
+
+import num2words
+
+import spacy
 
 from DataModel import DataModel
 
+from PredictionModel import limpiar_texto
+
 app = FastAPI()
+
+nlp = spacy.load("es_core_news_sm")
+
+def limpiar_ascii(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: unicodedata.normalize("NFKD", x).encode("ascii", "ignore").decode("utf-8"))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: unicodedata.normalize("NFKD", x).encode("ascii", "ignore").decode("utf-8"))
+
+    return
+
+
+def limpiar_mayusculas(train):
+
+    train["Titulo"] = train["Titulo"].str.lower()
+    train["Descripcion"] = train["Descripcion"].str.lower()
+
+    return
+
+
+def limpiar_puntuacion(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: re.sub(r'[.:,;-_¿?¡!"\'()\[\]{}\/%\$@+*\^]', '', x))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: re.sub(r'[.:,;-_¿?¡!"\'()\[\]{}\/%\$@+*\^]', '', x))
+    
+    return
+
+
+def limpiar_numeros(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: re.sub(r'\b\d+\b', lambda y: num2words.num2words(y.group(), lang='es'), x))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: re.sub(r'\b\d+\b', lambda y: num2words.num2words(y.group(), lang='es'), x))
+
+    return
+
+
+palabras_stop = nlp.Defaults.stop_words
+
+palabras_s = r'\b(?:' + '|'.join(palabras_stop) + r')\b'
+
+def limpiar_stopwords(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: re.sub(palabras_s, '', x))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: re.sub(palabras_s, '', x))
+
+    return
+
+def limpiar_guiones(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: x.replace("-", " "))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: x.replace("-", " "))
+
+    return
+
+numeros_post_ascii = r'[0123456789]'
+
+def limpiar_numeros_ascii(train):
+
+    train["Titulo"] = train["Titulo"].apply(lambda x: re.sub(numeros_post_ascii, '', x))
+    train["Descripcion"] = train["Descripcion"].apply(lambda x: re.sub(numeros_post_ascii, '', x))
+
+    return
+
+def limpiar_texto(train):
+
+    limpiar_ascii(train=train)
+    limpiar_mayusculas(train=train)
+    limpiar_puntuacion(train=train)
+    limpiar_numeros(train=train)
+    limpiar_stopwords(train=train)
+    limpiar_guiones(train=train)
+    limpiar_numeros_ascii(train=train)
+
+    return
+
+def tokenizar(train):
+
+    train["Titulo_T"] = train["Titulo"].swifter.allow_dask_on_strings(enable=True).apply(lambda x: [token.text for token in nlp(x) if not token.is_space])
+    train["Descripcion_T"] = train["Descripcion"].swifter.allow_dask_on_strings(enable=True).apply(lambda x: [token.text for token in nlp(x) if not token.is_space])
+
+    return train
+
+def unir_texto(train):
+
+    textos = train["Titulo_T"].astype(str) + " " + train["Descripcion_T"].astype(str)
+
+    return textos
+
+from joblib import load
 
 
 @app.get("/")
@@ -26,7 +122,7 @@ def read_item(item_id: int, q: Optional[str] = None):
 def make_predictions(data_list:List[DataModel]):
    
     df = pd.DataFrame([data.dict() for data in data_list])
-    model = load("assets/modelo.joblib")
+    model = load("model.joblib", mmap_mode="r")
     
     
     predictions = model.predict(df).tolist()
@@ -45,7 +141,7 @@ def make_prediction_arch(dataModel: DataModel):
     if not all(col in df.columns for col in expected_columns):
         raise HTTPException(status_code=400, detail=f"Las columnas deben ser {expected_columns}")
     
-    model = load("assets/modelo.joblib")
+    model = load("assets/model.joblib")
     
     predictions = model.predict(df[expected_columns])
     return {"predictions": predictions.tolist()}
@@ -63,7 +159,7 @@ def retrain(datamodel:DataModel):
    if not all(col in df.columns for col in expected_columns):
         raise HTTPException(status_code=400, detail=f"Las columnas deben ser {expected_columns}")
     
-   model = load("assets/modelo.joblib")
+   model = load("assets/model.joblib")
 
    predictions = model.fit_predict(df[["Titulo", "Descripcion"]],df["Label"])
    score = model.score(df[["Titulo", "Descripcion"]],df["Label"])
@@ -82,7 +178,7 @@ def adddata(datamodel:DataModel):
    if not all(col in df.columns for col in expected_columns):
         raise HTTPException(status_code=400, detail=f"Las columnas deben ser {expected_columns}")
     
-   model = load("assets/modelo.joblib")
+   model = load("assets/model.joblib")
    modelo_viejo = model
    modelo_nuevo = model.fit(df[["Titulo", "Descripcion"]],df["Label"])
    modelo_combinado = VotingClassifier(estimators=[('old', modelo_viejo), ('new', modelo_nuevo)],voting='soft')
